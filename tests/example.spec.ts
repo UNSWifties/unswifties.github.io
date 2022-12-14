7c09bd34aa4cbe313a8f51119b253fe58d5e548a
Will Kennedy <will.kennedy@wisetechglobal.com>
2022-12-15 01:46:13 +1100

    Hopefully working now

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('Is there an H1 tag on the homepage?', async ({ page }) => {
   await page.goto("/");
-  expect(page.locator('h1')).toHaveText('Hello, World!');
+  await expect(page.locator('h1')).toContainText('Hello, World!');
 });
