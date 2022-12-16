45939625578b96a68a563afa68d3259af313379d
Will Kennedy <will.kennedy@wisetechglobal.com>
2022-12-16 12:23:29 +1100

    Began creating Navigation component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import Navigation from "components/navigation"
+
+const PAGES : string[] = ["Home", "About", "Blog", "Contact"];
+
 export default function Home() {
-  return (<h1>Hello, World!</h1>)
+  return (
+    <Navigation links={PAGES}></Navigation>
+  )
 }
